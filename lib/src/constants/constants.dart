fd1c5e4e96bb2aa1b7af6756089df4a420a7550d
Daniel <daniel.torresg@udea.edu.co>
2020-12-02 17:51:37 -0500

    add constants

---
+++ lib/src/constants/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const purpleMainColor = Color(0x7444FB);
+const blueMainColor = Color(0x32D1F7);
+const grayComplementaryColor = Color(0x5C5E64);
+const purpleComplementaryColor = Color(0x2FB5E6);
+const blueComplementaryColor = Color(0x9C65F9);
+
