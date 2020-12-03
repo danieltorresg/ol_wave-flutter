78da0381dd11720b8ba78e42a6ba1a87e39fb441
Daniel <daniel.torresg@udea.edu.co>
2020-12-02 21:02:35 -0500

    Adding colors

--- lib/src/constants/constants.dart
+++ lib/src/constants/constants.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
-const purpleMainColor = Color(0x7444FB);
-const blueMainColor = Color(0x32D1F7);
-const grayComplementaryColor = Color(0x5C5E64);
-const purpleComplementaryColor = Color(0x2FB5E6);
-const blueComplementaryColor = Color(0x9C65F9);
+const purpleMainColor = Color(0xff7444fb);
+const blueMainColor = Color(0xff32D1F7);
+const grayComplementaryColor = Color(0xff5C5E64);
+const purpleComplementaryColor = Color(0xff2FB5E6);
+const blueComplementaryColor = Color(0xff9C65F9);
 
