194e3c40a57fb9505d3e77e1f7c118ea97c7fd47
mikestyles <duvankno3@gmail.com>
2020-12-03 16:08:39 -0500

    Material app structure

--- lib/src/view/splash_index.dart
+++ lib/src/view/splash_index.dart
@@ -2,6 +2,41 @@ import 'package:flutter/material.dart';
 import 'package:old_wave_flutter/src/view/splash_appbar.dart';
 import 'package:old_wave_flutter/src/view/splash_searchBar.dart';
 
+class DataQuemada {
+  static final getData = [
+    {
+      'name': 'Bitcoin',
+      'symbol': 'BTC',
+      'icon': Icons.account_box,
+      'iconColor': Colors.orange,
+      'change': '+3.67%',
+      'changeValue': '+202.835',
+      'changeColor': Colors.green,
+      'value': '\$12.279',
+    },
+    {
+      'name': 'Ethereum',
+      'symbol': 'ETH',
+      'icon': Icons.account_circle,
+      'iconColor': Colors.black,
+      'change': '+5.2%',
+      'changeValue': '25.567',
+      'changeColor': Colors.green,
+      'value': '\$7.809'
+    },
+    {
+      'name': 'Andrea',
+      'symbol': 'AOG',
+      'icon': Icons.android,
+      'iconColor': Colors.blueAccent,
+      'change': '+1.2%',
+      'changeValue': '20.567',
+      'changeColor': Colors.brown,
+      'value': '\$7.80900'
+    },
+  ];
+}
+
 class InitialPage extends StatefulWidget {
   InitialPage({Key key, this.title}) : super(key: key);
 
@@ -12,25 +47,25 @@ class InitialPage extends StatefulWidget {
 }
 
 class _InitialPageState extends State<InitialPage> {
+  var dataQuemada = DataQuemada.getData;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBarApp(
-        appBar: AppBar(),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            SearchBar(),
-            Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage('assets/oldwave-logo-vertical.png'),
-                    scale: 3),
-              ),
-            ),
-          ],
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBarApp(
+          appBar: AppBar(),
+        ),
+        body: Container(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                SearchBar(),
+                Expanded(
+                  child: ListView.builder(
+                      itemCount: dataQuemada.length,
+                      itemBuilder: (context, index) {}),
+                )
+              ]),
         ),
       ),
     );
