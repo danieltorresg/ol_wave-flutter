96a330af67d6a0cf01013d4de2ab29cdb6c7e66c
mikestyles <duvankno3@gmail.com>
2021-02-02 09:02:40 -0500

    widget tests

--- test/index_tets.dart
+++ test/index_tets.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:old_wave_flutter/src/view/index.dart';
+import 'package:old_wave_flutter/src/view/search.dart';
 
 void main() {
   testWidgets("Busqueda de un producto", (WidgetTester tester) async {
@@ -9,8 +10,15 @@ void main() {
 
     await tester.pumpWidget(MaterialApp(home: InitialPage()));
     await tester.enterText(busqueda, "Televisor");
-    await tester.pump();
+    //await tester.pump();
 
     expect(find.text("Televisor"), findsOneWidget);
+  });
+
+  testWidgets("Verificar que pase a la vista de search",
+      (WidgetTester tester) async {
+    final search = Search();
+    await tester.pumpWidget(Container(child: search));
+    expect(find.byWidget(search), findsOneWidget);
   });
 }
